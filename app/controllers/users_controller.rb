6202fcbea0119d1fdbd4cd15c44ecdec3d4b5a48
Sindhu Shree <sindhu20091@gmail.com>
2018-06-05 12:51:23 +0530

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   before_filter :admin_user,   :only => :destroy
   def show
     @user = User.find(params[:id])
-    @title = @user.name
+    @microposts = @user.microposts.paginate(:page => params[:page])
+    @title = CGI.escapeHTML(@user.name)
   end
 
   def index
@@ -50,10 +51,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def authenticate
-      deny_access unless signed_in?
-    end
 
     def correct_user
       @user = User.find(params[:id])
